a0d486889c22c2a24fe03e8cf9915e3e4c4ed0c5
Colin Drake <colin.f.drake@gmail.com>
2012-07-12 23:45:11 -0400

    Document API

--- gdbm.go
+++ gdbm.go
@@ -1,4 +1,5 @@
-// Packge gdbm implements a wrapper around libgdbm for Go.
+// Packge gdbm implements a wrapper around libgdbm, the GNU DataBase Manager
+// library, for Go.
 package gdbm
 
 // #cgo CFLAGS: -std=gnu99
@@ -19,13 +20,18 @@ import (
     "unsafe"
 )
 
-//
+// GDBM database "connection" type.
 type Database struct {
     dbf C.GDBM_FILE
     mode int
 }
 
-//
+// GDBM database configuration type that can be used to specify how the
+// database is created and treated. The `Mode` determines what the user of the
+// `Database` object can do with it. The field can be a string, either "r" for
+// Read-only, "w" for Read-Write, "c" for Read-Write and create if it doesn't
+// exist, and "n" for Read-Write and always recreate database, even if it
+// exists. TODO: write descriptions for other params... too lazy right now
 type DatabaseCfg struct {
     Mode string
     BlockSize int
@@ -42,10 +48,12 @@ func Open(filename string, mode string) (db * Database, err error) {
     return OpenWithCfg(filename, DatabaseCfg{mode, 0, 0666})
 }
 
+// More complex database initialization function that takes in a `DatabaseCfg`
+// struct to allow more fine-grained control over database settings.
 func OpenWithCfg(filename string, cfg DatabaseCfg) (db * Database, err error) {
     db = new(Database)
-    err = nil
 
+    // Convert a human-readable mode string into a libgdbm-usable constant.
     switch cfg.Mode {
     case "r": db.mode = C.GDBM_READER
     case "w": db.mode = C.GDBM_WRITER
@@ -55,7 +63,6 @@ func OpenWithCfg(filename string, cfg DatabaseCfg) (db * Database, err error) {
 
     cs := C.CString(filename)
     defer C.free(unsafe.Pointer(cs))
-
     db.dbf = C.gdbm_open(cs, C.int(cfg.BlockSize), C.int(db.mode), C.int(cfg.Permissions), nil)
     if db.dbf == nil {
         err = lastError()
@@ -63,13 +70,16 @@ func OpenWithCfg(filename string, cfg DatabaseCfg) (db * Database, err error) {
     return db, err
 }
 
-// Closes a database file.
+// Closes a database's internal file pointer.
 func (db * Database) Close() {
     C.gdbm_close(db.dbf)
 }
 
-//
+// Internal helper method to hide the two constants GDBM_INSERT and
+// GDBM_REPLACE from the user.
 func (db * Database) update(key string, value string, flag C.int) (err error) {
+    // Convert key and value into libgdbm's `datum` data structure. See the
+    // C definition at the top for the implementation of C.mk_datum(string).
     kcs := C.CString(key)
     vcs := C.CString(value)
     k := C.mk_datum(kcs)
@@ -84,16 +94,21 @@ func (db * Database) update(key string, value string, flag C.int) (err error) {
     return err
 }
 
-//
+// Inserts a key-value pair into the database. If the database is opened
+// in "r" mode, this will return an error. Also, if the key already exists in
+// the database, and error will be returned.
 func (db * Database) Insert(key string, value string) (err error) {
     return db.update(key, value, C.GDBM_INSERT)
 }
 
-//
+// Updates a key-value pair to use a new value, specified by the `value` string
+// parameter. An error will be returned if the database is opened in "r" mode.
 func (db * Database) Replace(key string, value string) (err error) {
     return db.update(key, value, C.GDBM_REPLACE)
 }
 
+// Returns true or false, depending on whether the specified key exists in the
+// database.
 func (db * Database) Exists(key string) bool {
     kcs := C.CString(key)
     k := C.mk_datum(kcs)
@@ -106,7 +121,9 @@ func (db * Database) Exists(key string) bool {
     return false
 }
 
-//
+// Fetches the value of the given key. If the key is not in the database, an
+// error will be returned in err. Otherwise, value will be the value string
+// that is keyed by `key`.
 func (db * Database) Fetch(key string) (value string, err error) {
     kcs := C.CString(key)
     k := C.mk_datum(kcs)
@@ -122,7 +139,8 @@ func (db * Database) Fetch(key string) (value string, err error) {
     return value, nil
 }
 
-//
+// Removes a key-value pair from the database. If the database is opened in "r"
+// mode, an error is returned
 func (db * Database) Delete(key string) (err error) {
     kcs := C.CString(key)
     k := C.mk_datum(kcs)
@@ -135,12 +153,14 @@ func (db * Database) Delete(key string) (err error) {
     return err
 }
 
-//
+// Reorganizes the database for more efficient use of disk space. This method
+// can be used if Delete(k) is called many times.
 func (db * Database) Reorganize() {
     C.gdbm_reorganize(db.dbf)
 }
 
-//
+// Synchronizes all pending database changes to the disk. TODO: note this is
+// only needed in FAST mode, and FAST mode needs implemented!
 func (db * Database) Sync() {
     C.gdbm_sync(db.dbf)
 }
