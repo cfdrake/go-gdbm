55545fc592e07e6558b282cf868e3540ed59bd9c
Colin Drake <colin.f.drake@gmail.com>
2012-07-12 23:51:43 -0400

    Give everything a good 'ole gofmt

--- gdbm.go
+++ gdbm.go
@@ -16,14 +16,14 @@ package gdbm
 import "C"
 
 import (
-    "errors"
-    "unsafe"
+	"errors"
+	"unsafe"
 )
 
 // GDBM database "connection" type.
 type Database struct {
-    dbf C.GDBM_FILE
-    mode int
+	dbf  C.GDBM_FILE
+	mode int
 }
 
 // GDBM database configuration type that can be used to specify how the
@@ -33,134 +33,138 @@ type Database struct {
 // exist, and "n" for Read-Write and always recreate database, even if it
 // exists. TODO: write descriptions for other params... too lazy right now
 type DatabaseCfg struct {
-    Mode string
-    BlockSize int
-    Permissions int
+	Mode        string
+	BlockSize   int
+	Permissions int
 }
 
 func lastError() error {
-    return errors.New(C.GoString(C.gdbm_strerror(C.gdbm_errno)))
+	return errors.New(C.GoString(C.gdbm_strerror(C.gdbm_errno)))
 }
 
 // Simple function to open a database file with default parameters (block size
 // is default for the filesystem and file permissions are set to 0666).
-func Open(filename string, mode string) (db * Database, err error) {
-    return OpenWithCfg(filename, DatabaseCfg{mode, 0, 0666})
+func Open(filename string, mode string) (db *Database, err error) {
+	return OpenWithCfg(filename, DatabaseCfg{mode, 0, 0666})
 }
 
 // More complex database initialization function that takes in a `DatabaseCfg`
 // struct to allow more fine-grained control over database settings.
-func OpenWithCfg(filename string, cfg DatabaseCfg) (db * Database, err error) {
-    db = new(Database)
+func OpenWithCfg(filename string, cfg DatabaseCfg) (db *Database, err error) {
+	db = new(Database)
 
-    // Convert a human-readable mode string into a libgdbm-usable constant.
-    switch cfg.Mode {
-    case "r": db.mode = C.GDBM_READER
-    case "w": db.mode = C.GDBM_WRITER
-    case "c": db.mode = C.GDBM_WRCREAT
-    case "n": db.mode = C.GDBM_NEWDB
-    }
+	// Convert a human-readable mode string into a libgdbm-usable constant.
+	switch cfg.Mode {
+	case "r":
+		db.mode = C.GDBM_READER
+	case "w":
+		db.mode = C.GDBM_WRITER
+	case "c":
+		db.mode = C.GDBM_WRCREAT
+	case "n":
+		db.mode = C.GDBM_NEWDB
+	}
 
-    cs := C.CString(filename)
-    defer C.free(unsafe.Pointer(cs))
-    db.dbf = C.gdbm_open(cs, C.int(cfg.BlockSize), C.int(db.mode), C.int(cfg.Permissions), nil)
-    if db.dbf == nil {
-        err = lastError()
-    }
-    return db, err
+	cs := C.CString(filename)
+	defer C.free(unsafe.Pointer(cs))
+	db.dbf = C.gdbm_open(cs, C.int(cfg.BlockSize), C.int(db.mode), C.int(cfg.Permissions), nil)
+	if db.dbf == nil {
+		err = lastError()
+	}
+	return db, err
 }
 
 // Closes a database's internal file pointer.
-func (db * Database) Close() {
-    C.gdbm_close(db.dbf)
+func (db *Database) Close() {
+	C.gdbm_close(db.dbf)
 }
 
 // Internal helper method to hide the two constants GDBM_INSERT and
 // GDBM_REPLACE from the user.
-func (db * Database) update(key string, value string, flag C.int) (err error) {
-    // Convert key and value into libgdbm's `datum` data structure. See the
-    // C definition at the top for the implementation of C.mk_datum(string).
-    kcs := C.CString(key)
-    vcs := C.CString(value)
-    k := C.mk_datum(kcs)
-    v := C.mk_datum(vcs)
-    defer C.free(unsafe.Pointer(kcs))
-    defer C.free(unsafe.Pointer(vcs))
+func (db *Database) update(key string, value string, flag C.int) (err error) {
+	// Convert key and value into libgdbm's `datum` data structure. See the
+	// C definition at the top for the implementation of C.mk_datum(string).
+	kcs := C.CString(key)
+	vcs := C.CString(value)
+	k := C.mk_datum(kcs)
+	v := C.mk_datum(vcs)
+	defer C.free(unsafe.Pointer(kcs))
+	defer C.free(unsafe.Pointer(vcs))
 
-    retv := C.gdbm_store(db.dbf, k, v, flag)
-    if retv != 0 {
-        err = lastError()
-    }
-    return err
+	retv := C.gdbm_store(db.dbf, k, v, flag)
+	if retv != 0 {
+		err = lastError()
+	}
+	return err
 }
 
 // Inserts a key-value pair into the database. If the database is opened
 // in "r" mode, this will return an error. Also, if the key already exists in
 // the database, and error will be returned.
-func (db * Database) Insert(key string, value string) (err error) {
-    return db.update(key, value, C.GDBM_INSERT)
+func (db *Database) Insert(key string, value string) (err error) {
+	return db.update(key, value, C.GDBM_INSERT)
 }
 
 // Updates a key-value pair to use a new value, specified by the `value` string
 // parameter. An error will be returned if the database is opened in "r" mode.
-func (db * Database) Replace(key string, value string) (err error) {
-    return db.update(key, value, C.GDBM_REPLACE)
+func (db *Database) Replace(key string, value string) (err error) {
+	return db.update(key, value, C.GDBM_REPLACE)
 }
 
 // Returns true or false, depending on whether the specified key exists in the
 // database.
-func (db * Database) Exists(key string) bool {
-    kcs := C.CString(key)
-    k := C.mk_datum(kcs)
-    defer C.free(unsafe.Pointer(kcs))
+func (db *Database) Exists(key string) bool {
+	kcs := C.CString(key)
+	k := C.mk_datum(kcs)
+	defer C.free(unsafe.Pointer(kcs))
 
-    e := C.gdbm_exists(db.dbf, k)
-    if e == 1 {
-        return true
-    }
-    return false
+	e := C.gdbm_exists(db.dbf, k)
+	if e == 1 {
+		return true
+	}
+	return false
 }
 
 // Fetches the value of the given key. If the key is not in the database, an
 // error will be returned in err. Otherwise, value will be the value string
 // that is keyed by `key`.
-func (db * Database) Fetch(key string) (value string, err error) {
-    kcs := C.CString(key)
-    k := C.mk_datum(kcs)
-    defer C.free(unsafe.Pointer(kcs))
+func (db *Database) Fetch(key string) (value string, err error) {
+	kcs := C.CString(key)
+	k := C.mk_datum(kcs)
+	defer C.free(unsafe.Pointer(kcs))
 
-    vdatum := C.gdbm_fetch(db.dbf, k)
-    if vdatum.dptr == nil {
-        return "", lastError()
-    }
+	vdatum := C.gdbm_fetch(db.dbf, k)
+	if vdatum.dptr == nil {
+		return "", lastError()
+	}
 
-    value = C.GoString(vdatum.dptr)
-    defer C.free(unsafe.Pointer(vdatum.dptr))
-    return value, nil
+	value = C.GoString(vdatum.dptr)
+	defer C.free(unsafe.Pointer(vdatum.dptr))
+	return value, nil
 }
 
 // Removes a key-value pair from the database. If the database is opened in "r"
 // mode, an error is returned
-func (db * Database) Delete(key string) (err error) {
-    kcs := C.CString(key)
-    k := C.mk_datum(kcs)
-    defer C.free(unsafe.Pointer(kcs))
+func (db *Database) Delete(key string) (err error) {
+	kcs := C.CString(key)
+	k := C.mk_datum(kcs)
+	defer C.free(unsafe.Pointer(kcs))
 
-    retv := C.gdbm_delete(db.dbf, k)
-    if retv == -1 && db.mode == C.GDBM_READER {
-        err = lastError()
-    }
-    return err
+	retv := C.gdbm_delete(db.dbf, k)
+	if retv == -1 && db.mode == C.GDBM_READER {
+		err = lastError()
+	}
+	return err
 }
 
 // Reorganizes the database for more efficient use of disk space. This method
 // can be used if Delete(k) is called many times.
-func (db * Database) Reorganize() {
-    C.gdbm_reorganize(db.dbf)
+func (db *Database) Reorganize() {
+	C.gdbm_reorganize(db.dbf)
 }
 
 // Synchronizes all pending database changes to the disk. TODO: note this is
 // only needed in FAST mode, and FAST mode needs implemented!
-func (db * Database) Sync() {
-    C.gdbm_sync(db.dbf)
+func (db *Database) Sync() {
+	C.gdbm_sync(db.dbf)
 }
--- gdbm_test.go
+++ gdbm_test.go
@@ -1,116 +1,116 @@
 package gdbm
 
 import (
-    "os"
-    "testing"
+	"os"
+	"testing"
 )
 
 // Tests that the database is recreated everytime when opened in "c" mode.
 // Ensures that the file exists and that there are no key-value pairs.
-func TestRecreate(t * testing.T) {
-    db, err := Open("test.gdbm", "c")
-    defer db.Close()
+func TestRecreate(t *testing.T) {
+	db, err := Open("test.gdbm", "c")
+	defer db.Close()
 
-    if err != nil {
-        t.Error("Couldn't create new database")
-    }
+	if err != nil {
+		t.Error("Couldn't create new database")
+	}
 
-    f, err := Open("test.gdbm", "r")
-    defer f.Close()
+	f, err := Open("test.gdbm", "r")
+	defer f.Close()
 
-    if os.IsExist(err) {
-        t.Error("Database wasn't actually created")
-    }
+	if os.IsExist(err) {
+		t.Error("Database wasn't actually created")
+	}
 
-    // TODO: test for no keys
+	// TODO: test for no keys
 }
 
 // TODO: test other initializers
 
 // Ensure that the database doesn't allow a Reader type to call an database-
 // modifying operation such as Insert().
-func TestWriteErrorWhenReader(t * testing.T) {
-    // Ensure the db exists for this test
-    db, err := Open("test.gdbm", "c")
-    db.Close()
+func TestWriteErrorWhenReader(t *testing.T) {
+	// Ensure the db exists for this test
+	db, err := Open("test.gdbm", "c")
+	db.Close()
 
-    db, err = Open("test.gdbm", "r")
-    defer db.Close()
+	db, err = Open("test.gdbm", "r")
+	defer db.Close()
 
-    if err != nil {
-        t.Error("Couldn't read database")
-    }
+	if err != nil {
+		t.Error("Couldn't read database")
+	}
 
-    err = db.Insert("foo", "bar")
-    if err == nil {
-        t.Error("Database let readonly client write")
-    }
+	err = db.Insert("foo", "bar")
+	if err == nil {
+		t.Error("Database let readonly client write")
+	}
 }
 
 // Tests to make sure that inserting a key, then replacing its old value with a
 // new one will not return an error (as would happen if you were to call
 // Insert() again rather than Replace()) and that the key is actually updated.
-func TestReplace(t * testing.T) {
-    db, err := Open("test.gdbm", "c")
-    defer db.Close()
+func TestReplace(t *testing.T) {
+	db, err := Open("test.gdbm", "c")
+	defer db.Close()
 
-    if err != nil {
-        t.Error("Couldn't create database")
-    }
+	if err != nil {
+		t.Error("Couldn't create database")
+	}
 
-    err = db.Insert("foo", "bar")
-    err = db.Replace("foo", "biz")
-    key, err := db.Fetch("foo")
-    if err != nil || key != "biz" {
-        t.Error("Replace didn't update key correctly")
-    }
+	err = db.Insert("foo", "bar")
+	err = db.Replace("foo", "biz")
+	key, err := db.Fetch("foo")
+	if err != nil || key != "biz" {
+		t.Error("Replace didn't update key correctly")
+	}
 }
 
 // Ensures that if a key k is inserted into the database, calling Exists(k)
 // returns true - indicating that the insertion was successful.
-func TestExists(t * testing.T) {
-    db, err := Open("test.gdbm", "c")
-    defer db.Close()
+func TestExists(t *testing.T) {
+	db, err := Open("test.gdbm", "c")
+	defer db.Close()
 
-    if err != nil {
-        t.Error("Couldn't create database")
-    }
+	if err != nil {
+		t.Error("Couldn't create database")
+	}
 
-    err = db.Insert("foo", "bar")
-    exists := db.Exists("foo")
-    if !exists {
-        t.Error("Inserted key reported as not existing")
-    }
+	err = db.Insert("foo", "bar")
+	exists := db.Exists("foo")
+	if !exists {
+		t.Error("Inserted key reported as not existing")
+	}
 }
 
 // Test to make sure that if a key is inserted then deleted, that it no longer
 // returns true for Exists(key).
-func TestDelete(t * testing.T) {
-    db, err := Open("test.gdbm", "c")
-    defer db.Close()
+func TestDelete(t *testing.T) {
+	db, err := Open("test.gdbm", "c")
+	defer db.Close()
 
-    db.Insert("foo", "bar")
-    err = db.Delete("foo")
-    exists := db.Exists("foo")
-    if err != nil || exists {
-        t.Error("Delete()ed key not removed")
-    }
+	db.Insert("foo", "bar")
+	err = db.Delete("foo")
+	exists := db.Exists("foo")
+	if err != nil || exists {
+		t.Error("Delete()ed key not removed")
+	}
 }
 
 // Ensure the Reorganize() function doesn't halt execution.
-func TestReorganize(t * testing.T) {
-    // No values to test, but testing as a sanity test - if something does go
-    // wrong and the program crashes, this'll let us know Reorganize() is
-    // broken. ;)
-    db, _ := Open("test.gdbm", "c")
-    defer db.Close()
-    db.Reorganize()
+func TestReorganize(t *testing.T) {
+	// No values to test, but testing as a sanity test - if something does go
+	// wrong and the program crashes, this'll let us know Reorganize() is
+	// broken. ;)
+	db, _ := Open("test.gdbm", "c")
+	defer db.Close()
+	db.Reorganize()
 }
 
 // Ensure the Sync() function doesn't halt execution.
-func TestSync(t * testing.T) {
-    // Same as TestReorganize() w/r/t no return value, etc.
-    db, _ := Open("test.gdbm", "c")
-    defer db.Close()
-    db.Sync()
+func TestSync(t *testing.T) {
+	// Same as TestReorganize() w/r/t no return value, etc.
+	db, _ := Open("test.gdbm", "c")
+	defer db.Close()
+	db.Sync()
 }
