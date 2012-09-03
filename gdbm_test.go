cf8170e5a6e79f665d2003e07743c9cc2f9c6767
Vincent Batts <vbatts@redhat.com>
2012-09-03 09:56:20 -0400

    adding some cleanup to the tests.  * single filename  * remove file after test

--- gdbm_test.go
+++ gdbm_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 )
 
+var db_filename string = "test.gdbm"
+
 // Test that GDBM version is returned
 func TestVersion(t *testing.T) {
 	vers := Version()
@@ -16,15 +18,16 @@ func TestVersion(t *testing.T) {
 // Tests that the database is recreated everytime when opened in "c" mode.
 // Ensures that the file exists and that there are no key-value pairs.
 func TestRecreate(t *testing.T) {
-	db, err := Open("test.gdbm", "c")
+	db, err := Open(db_filename, "c")
 	defer db.Close()
 
 	if err != nil {
 		t.Error("Couldn't create new database")
 	}
 
-	f, err := Open("test.gdbm", "r")
+	f, err := Open(db_filename, "r")
 	defer f.Close()
+	defer os.Remove(db_filename)
 
 	if os.IsExist(err) {
 		t.Error("Database wasn't actually created")
@@ -39,11 +42,12 @@ func TestRecreate(t *testing.T) {
 // modifying operation such as Insert().
 func TestWriteErrorWhenReader(t *testing.T) {
 	// Ensure the db exists for this test
-	db, err := Open("test.gdbm", "c")
+	db, err := Open(db_filename, "c")
 	db.Close()
 
-	db, err = Open("test.gdbm", "r")
+	db, err = Open(db_filename, "r")
 	defer db.Close()
+	defer os.Remove(db_filename)
 
 	if err != nil {
 		t.Error("Couldn't read database")
@@ -59,8 +63,9 @@ func TestWriteErrorWhenReader(t *testing.T) {
 // new one will not return an error (as would happen if you were to call
 // Insert() again rather than Replace()) and that the key is actually updated.
 func TestReplace(t *testing.T) {
-	db, err := Open("test.gdbm", "c")
+	db, err := Open(db_filename, "c")
 	defer db.Close()
+	defer os.Remove(db_filename)
 
 	if err != nil {
 		t.Error("Couldn't create database")
@@ -77,8 +82,9 @@ func TestReplace(t *testing.T) {
 // Ensures that if a key k is inserted into the database, calling Exists(k)
 // returns true - indicating that the insertion was successful.
 func TestExists(t *testing.T) {
-	db, err := Open("test.gdbm", "c")
+	db, err := Open(db_filename, "c")
 	defer db.Close()
+	defer os.Remove(db_filename)
 
 	if err != nil {
 		t.Error("Couldn't create database")
@@ -94,8 +100,9 @@ func TestExists(t *testing.T) {
 // Test to make sure that if a key is inserted then deleted, that it no longer
 // returns true for Exists(key).
 func TestDelete(t *testing.T) {
-	db, err := Open("test.gdbm", "c")
+	db, err := Open(db_filename, "c")
 	defer db.Close()
+	defer os.Remove(db_filename)
 
 	db.Insert("foo", "bar")
 	err = db.Delete("foo")
@@ -110,15 +117,19 @@ func TestReorganize(t *testing.T) {
 	// No values to test, but testing as a sanity test - if something does go
 	// wrong and the program crashes, this'll let us know Reorganize() is
 	// broken. ;)
-	db, _ := Open("test.gdbm", "c")
+	db, _ := Open(db_filename, "c")
 	defer db.Close()
+	defer os.Remove(db_filename)
+
 	db.Reorganize()
 }
 
 // Ensure the Sync() function doesn't halt execution.
 func TestSync(t *testing.T) {
 	// Same as TestReorganize() w/r/t no return value, etc.
-	db, _ := Open("test.gdbm", "c")
+	db, _ := Open(db_filename, "c")
 	defer db.Close()
+	defer os.Remove(db_filename)
+
 	db.Sync()
 }
